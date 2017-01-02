06a05caaec7c9ae369a85a85ea2915fa6f616d52
Sean Nam <snam@Seans-MacBook-Pro.local>
2017-01-02 00:10:54 -0800

    connect to server and added regex to check for whitespace

--- server.js
+++ server.js
@@ -5,7 +5,26 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
 	if(err) throw err;
 
 	client.on('connection', function(socket) {
-		console.log('Someone has connected');
+		//console.log('Someone has connected');
+
+		var col = db.collection('messages');
+
+		// Wait for input
+		socket.on('input', function(data) {
+			//console.log(data);
+
+			var name = data.name,
+				message = data.message,
+				whitespacePattern = /^\s*$/;
+
+			if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
+				console.log('Invalid');
+			} else {
+				col.insert({name: name, message: message}, function() {
+					console.log('inserted');
+				});
+			}
+		});
 	});
 });
 
